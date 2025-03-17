a1346ac5f941b35d76ebdcf613b43be571a4b753
陳吟甄 Chen Yin Zhen <yinzhenchen@iii.org.tw>
2025-03-17 17:47:57 +0800

    刪除養液資料

--- flask/new_database.sql
+++ flask/new_database.sql
@@ -146,10 +146,6 @@ JOIN users u ON f.user_id = u.id
 
 WHERE u.username = 'farmer1';
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 3ea3e11c18ac0d316d9885e95b7fb48691767418
 -- form06（肥料施用紀錄）
 CREATE TABLE form06 (
     id                 INT AUTO_INCREMENT PRIMARY KEY,  -- 唯一編號
